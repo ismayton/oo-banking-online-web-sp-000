7109516c6c8dbdfe3238d820a05bb3829a47683a
Ian Mayton <ian.mayton@gmail.com>
2020-07-28 15:41:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+  
+  attr_reader :status, :sender, :receiver, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
 end
